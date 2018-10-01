8d63b47a04d3841e89a359928f9ad27c214822ed
Antoine van Gelder <antoine@trigger.io>
2018-10-01 15:56:41 +0200

    Add a simple stress test to the unit tests

--- module/tests/automated.js
+++ module/tests/automated.js
@@ -1,4 +1,4 @@
-/* global module, asyncTest, ok, start */
+/* global module, asyncTest, ok, start, $, forge */
 
 module("forge.httpd");
 
@@ -13,11 +13,41 @@ asyncTest("Test that content is being served via https", 1, function () {
 });
 
 asyncTest("Test that content is being served from localhost", 1, function () {
-    if (window.location.hostname == "toolkit-local.com") {
-        ok(true, "Expected 'toolkit-local.com'");
+    if (window.location.hostname == "localhost") {
+        ok(true, "Expected 'localhost'");
         start();
     } else {
-        ok(false, "Expected 'toolkit-local.com', got: " + window.location.hostname);
+        ok(false, "Expected 'localhost', got: " + window.location.hostname);
         start();
+    }
+});
+
+var numrequests = 512;
+
+asyncTest("Test that we can make a lot of requests", numrequests, function () {
+    var url = window.location.protocol + "//" +
+              window.location.host + "/src/fixtures/httpd/icon-512.png";
+    forge.logging.log("Using url: " + url);
+    var count = 0;
+    var options = {
+        url: url,
+        success: function () {
+            count++;
+            ok(true);
+            if (count === numrequests) {
+                start();
+            }
+        },
+        error: function (error) {
+            count++;
+            ok(false, "Error: " + JSON.stringify(error));
+            if (count === numrequests) {
+                start();
+            }
+        }
+    };
+    for (var i = 0; i < numrequests; i++) {
+        options.url = url + "?count=" + i;
+        $.ajax(options);
     }
 });
